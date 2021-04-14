5b470923c6a910bb8e98752fbab106589248fc59
yanhongguo <w_yanhongguo@lhwork.net>
2021-04-14 14:48:27 +0800

    aa

--- common/src/main/java/CommonValue.java
+++ common/src/main/java/CommonValue.java
@@ -7,6 +7,12 @@ public class CommonValue {
 
         Integer c = 8;
 
+        Integer d =8;
+
+        Integer e=1000;
+
+        Integer f=1000;
+
         System.out.println(a.equals(b));
 
         System.out.println(a.equals(c));
@@ -18,6 +24,10 @@ public class CommonValue {
         System.out.println(a == c);
 
         System.out.println(b == c);
+
+        System.out.println(c==d);
+
+        System.out.println(e==f);
 
     }
 
--- common/src/main/java/thread/ThreadPoolTest1.java
+++ common/src/main/java/thread/ThreadPoolTest1.java
@@ -7,25 +7,28 @@ public class ThreadPoolTest1 {
 
         public static void main(String[] args) {
             BlockingQueue<Runnable> queue = new ArrayBlockingQueue<>(100);
+            Executors.newFixedThreadPool(1);
             ThreadFactory factory = r -> {
                 Thread thread = new Thread(r, "test-thread-pool");
-                thread.setDaemon(true);
+                //thread.setDaemon(true);
                 return thread;
             };
             ThreadPoolExecutor executor = new ThreadPoolExecutor(5, 10,
-                    1L, TimeUnit.SECONDS, queue, factory, new ThreadPoolExecutor.AbortPolicy());
-            System.out.println("活动的线程数"+executor.getPoolSize());
-            System.out.println("活动的线程数"+executor.getActiveCount());
+                    0L, TimeUnit.SECONDS, queue, factory, new ThreadPoolExecutor.DiscardPolicy());
+            System.out.println("aa线程池中的线程数"+executor.getPoolSize());
+            System.out.println("aa活动的线程数"+executor.getActiveCount());
+            executor.allowCoreThreadTimeOut(false);
             AtomicInteger i=new AtomicInteger(0);
             CountDownLatch count=new CountDownLatch(80);
             while (true) {
                 i.incrementAndGet();
-                if(i.get()==81)break;
+                if(i.get()==150)break;
                 executor.submit(() -> {
                     try {
                         System.out.println(i);
-                        Thread.sleep(300);
-                    } catch (InterruptedException e) {
+                        System.out.println("bb线程活动的线程数"+executor.getPoolSize());
+                        System.out.println("bb活动的线程数"+executor.getActiveCount());
+                    } catch (Exception e) {
                         e.printStackTrace();
                     }
                     //count.countDown();
@@ -42,7 +45,7 @@ public class ThreadPoolTest1 {
 //            } catch (InterruptedException e) {
 //                e.printStackTrace();
 //            }
-            executor.shutdown();
+            //executor.shutdown();
             System.out.println("活动的线程数"+executor.getPoolSize());
             System.out.println("活动的线程数"+executor.getActiveCount());
 
